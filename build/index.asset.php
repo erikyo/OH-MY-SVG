5c11a826032b30879bebf995b8aa1ce8203382bb
Erik Golinelli <erik@codekraft.it>
2022-07-15 19:08:04 +0200

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0dbdd55a3c41301a3eeb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd6c4705c062d08311751');
