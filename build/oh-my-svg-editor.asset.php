ba0be1d810e30031fc79d145d2fd42d824696feb
Erik Golinelli <erik@codekraft.it>
2022-08-30 19:55:59 +0200

    deps update + build

--- build/oh-my-svg-editor.asset.php
+++ build/oh-my-svg-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8fa2d2a36ffcf179652c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cffc90870a222c91ffb1');
