5d7b97ebd6101a03483fea46d0f6e5756eb7b552
Erik Golinelli <erik@codekraft.it>
2023-01-29 17:17:37 +0100

    svg component support for extra styles

--- src/Svg.tsx
+++ src/Svg.tsx
@@ -1,36 +1,28 @@
 import { updateHtmlProp, cleanMarkup } from './utils/common';
-import { svgAttributesDef } from './types';
+import { SvgAttributesSave } from './types';
 import classnames from 'classnames';
-import { transform } from '@babel/core';
 
 /**
  * Svg component - it can be used to render SVG files
  *
- * @param  attributes
- * @return {JSX.Element|null} the SVG component
+ * @param  props
+ * @return {JSX.Element|null} the SVG components
  */
-const SVG = ( attributes: svgAttributesDef ): JSX.Element | null => {
-	const { markup, width, height, rotation, className, style, filename } =
-		attributes;
+const SVG = ( props: SvgAttributesSave ): JSX.Element | null => {
+	const { svg, width, height, rotation, className, style } = props;
 
-	const svgDoc = updateHtmlProp( markup, [
+	const svgDoc = updateHtmlProp( svg, [
 		{ prop: 'width', value: width || '100%' },
 		{ prop: 'height', value: height || false },
 	] );
 
-	const cssTransforms = rotation
-		? { transform: `:rotate(${ rotation }deg)` }
-		: {};
-	const styles = {
-		...cssTransforms,
-		...style,
-	};
-
 	return svgDoc ? (
 		<div
-			className={ classnames( className ) }
-			data-svg={ filename || null }
-			style={ styles }
+			className={ className }
+			style={ {
+				...style,
+				transform: `rotate(${ rotation }deg)`,
+			} }
 			dangerouslySetInnerHTML={ cleanMarkup( svgDoc ) }
 		/>
 	) : null;
