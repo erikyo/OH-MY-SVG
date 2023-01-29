0e1d226a4c1434783632023763a8d122001026b5
Erik Golinelli <erik@codekraft.it>
2023-01-29 17:18:47 +0100

    updated save function in order to support new props (styles, border...)

--- src/save.tsx
+++ src/save.tsx
@@ -4,44 +4,68 @@ import {
 	__experimentalGetBorderClassesAndStyles as getBorderClassesAndStyles,
 } from '@wordpress/block-editor';
 import { hasAlign } from './utils/fn';
+import { SvgAttributesDef, SvgAttributesSave } from './types';
+import { BlockSaveProps } from '@wordpress/blocks';
+import { SVG_MIN_SIZE } from './constants';
 import classnames from 'classnames';
-import { svgAttributesDef } from './types';
 
 /**
  * @module Save
  * @description The svg save function.
  *
  * @param                    props
- * @param {svgAttributesDef} props.attributes - The props to build the svg
+ * @param {SvgAttributesDef} props.attributes - The props to build the svg
  *
  * @return {JSX.Element} - Returns an anchor tag if the url attribute is set, otherwise it returns a div tag the collection of attributes needed for saving the svg as xml markup
  */
-export const Save = ( props: {
-	attributes: svgAttributesDef;
-} ): JSX.Element => {
-	const { svg, url, width, height, rotation, align, style, className } =
-		props.attributes;
+export const Save = (
+	props: BlockSaveProps< SvgAttributesSave >
+): JSX.Element => {
+	const {
+		href,
+		linkTarget,
+		width,
+		height,
+		rotation,
+		align,
+		style,
+		svg,
+		className,
+	} = props.attributes;
 
 	const borderProps = getBorderClassesAndStyles( props.attributes );
-	const blockProps: Record< string, unknown > = useBlockProps.save( {
-		rotation,
+
+	const blockProps = useBlockProps.save( {
 		style: {
 			...style,
 			...borderProps.style,
-			width: hasAlign( align, [ 'full', 'wide' ] ) ? '100%' : null,
 			display: hasAlign( align, 'center' ) ? 'table' : null,
+			width: hasAlign( align, [ 'full', 'wide' ] ) ? '100%' : null,
 		},
-		width: ! hasAlign( align, [ 'full', 'wide' ] ) ? width : false,
-		height: ! hasAlign( align, [ 'full', 'wide' ] ) ? height : false,
-		className: classnames( className, borderProps.class ),
-		markup: svg,
+		className: classnames( className, borderProps.className ),
 	} );
 
-	return url ? (
-		<a href={ url }>
-			<SVG { ...blockProps as svgAttributesDef } />
+	const target = linkTarget ? { target: linkTarget } : '';
+
+	const svgTag: JSX.Element = (
+		<SVG
+			{ ...blockProps }
+			svg={ svg }
+			width={
+				! hasAlign( align, [ 'full', 'wide' ] ) ? width : SVG_MIN_SIZE
+			}
+			height={
+				! hasAlign( align, [ 'full', 'wide' ] ) ? height : SVG_MIN_SIZE
+			}
+			rotation={ rotation }
+		/>
+	);
+
+	return href ? (
+		<a href={ href } { ...target }>
+			{ svgTag }
 		</a>
 	) : (
-		<SVG { ...blockProps as svgAttributesDef } />
+		<>{ svgTag }</>
 	);
 };
