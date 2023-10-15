f4052ba1015686e7d6cecc4b0ad243d60b1fc088
Erik Golinelli <erik@codekraft.it>
2023-10-15 14:00:16 +0200

    the svg markup component
    Took 46 seconds

--- src/components/SvgEl.tsx
+++ src/components/SvgEl.tsx
@@ -1,4 +1,5 @@
-import { updateHtmlProp, cleanMarkup } from './utils/common';
+import { updateHtmlProp, cleanMarkup } from '../utils/common';
+import { hasAlign } from '../utils/svgTools';
 
 /**
  * Svg component - it can be used to render SVG files
@@ -6,11 +7,18 @@ import { updateHtmlProp, cleanMarkup } from './utils/common';
  * @param  attributes
  * @return { string | null } the SVG components
  */
-const getSVG = ( attributes: any ): { __html: string } => {
-	const { svg, width, height, rotation } = attributes;
+const SvgEl = ( attributes: any ): { __html: string } => {
+	const { svg, width, height, rotation, align } = attributes;
 
 	const svgStyle =
 		Number( rotation ) !== 0 ? `transform:rotate(${ rotation }deg)` : null;
+
+	const svgWidth =
+		width && hasAlign( align, [ 'full', 'wide', 'none' ] ) ? width : '100%';
+	const svgHeight =
+		height && hasAlign( align, [ 'full', 'wide', 'none' ] )
+			? height
+			: '100%';
 
 	const svgDoc = updateHtmlProp( svg, [
 		{ prop: 'width', value: width || '100%' },
@@ -21,4 +29,4 @@ const getSVG = ( attributes: any ): { __html: string } => {
 	return cleanMarkup( svgDoc );
 };
 
-export default getSVG;
+export default SvgEl;
