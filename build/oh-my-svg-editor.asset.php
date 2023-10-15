58c87374501a92a614c716f2d8d22debe69ec465
Erik Golinelli <erik@codekraft.it>
2023-10-15 13:51:09 +0200

    chore
    Took 9 hours 35 minutes

--- build/oh-my-svg-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'deac9d9b1696c7bd8068');
