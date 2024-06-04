a3264f8963cc807cfaa55baffb0580b673cb029e
Erik Golinelli <erik@codekraft.it>
2024-06-04 13:26:36 +0200

    deps updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,7 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const path = require( 'path' );
 
 module.exports = {
 	...defaultConfig,
-	entry: {
-		'oh-my-svg-editor': path.resolve( process.cwd(), `src/index.ts` ),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.[tj]sx?$/,
-				use: [ 'babel-loader' ],
-				exclude: /node_modules/,
-			},
-		],
-		...defaultConfig.module,
-	},
 	resolve: {
 		...defaultConfig.resolve,
 		fallback: {
