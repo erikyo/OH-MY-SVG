93be808c81363cc7153faf73b5f4a51c04b8b19a
Erik Golinelli <erik@codekraft.it>
2026-02-21 10:14:53 +0100

    refactor: update save.tsx to enhance SVG rendering logic, improve attribute handling, and align with updated utilities

--- src/save.tsx
+++ src/save.tsx
@@ -1,50 +1,104 @@
-import OHMYSVG from './components/SVG';
+import getSVG from './Svg';
+import classnames from 'classnames';
 
-import {
-	__experimentalGetElementClassName as getElementClassName,
-	__experimentalGetBorderClassesAndStyles as getBorderClassesAndStyles,
-	__experimentalGetShadowClassesAndStyles as getShadowClassesAndStyles,
-	useBlockProps,
-} from '@wordpress/block-editor';
-import { SvgAttributesSave } from './types';
-import {
-	getRotationCss,
-	getWrapperProps,
-	updateSvgMarkup,
-} from './utils/svgTools';
+import { useBlockProps } from '@wordpress/block-editor';
+import { hasAlign } from './utils/fn';
+import type { SvgAttributesSave } from './types';
+
+// Extend attributes interface to include mediaUrl
+interface ExtendedAttributes extends SvgAttributesSave {
+	mediaUrl?: string;
+	storage?: 'inline' | 'media';
+}
+import { NEW_TAB_REL } from './constants';
+import { getAlignStyle } from './utils/presets';
 
 /**
  * @module Save
  * @description The svg save function.
  *
- * @param                     props.SvgAttributesSave
- * @param                     props
- * @param {SvgAttributesSave} props.attributes        - The props to build the svg
+ * @param                    props
+ * @param {SvgAttributesDef} props.attributes - The props to build the svg
  *
  * @return {JSX.Element} - Returns an anchor tag if the url attribute is set, otherwise it returns a div tag the collection of attributes needed for saving the svg as xml markup
  */
-export const Save = ( props: {
+export const Save = (props: {
 	attributes: SvgAttributesSave;
-} ): JSX.Element => {
-	const attributes = props.attributes;
+}): JSX.Element => {
+	const {
+		href,
+		linkTarget,
+		width,
+		height,
+		title,
+		rotation,
+		align,
+		onclick,
+	} = props.attributes;
 
-	const wrapperProps = getWrapperProps( props.attributes, {
-		...getBorderClassesAndStyles( attributes ),
-		...getShadowClassesAndStyles( attributes ),
-	} );
+	const blockProps = useBlockProps.save({
+		style: {
+			...getAlignStyle(align),
+		},
+		className: classnames(
+			align ? `align${align}` : 'alignnone',
+		),
+	});
 
-	const svgMarkup = updateSvgMarkup( attributes );
-	const blockProps = useBlockProps.save();
+	const SvgTag = () => {
+		const style = {
+			...getAlignStyle(align),
+			margin: hasAlign(align, 'center') ? 'auto' : undefined,
+			transform:
+				Number(rotation) !== 0
+					? `rotate( ${rotation}deg )`
+					: undefined,
+		};
+		const widthAttr = !hasAlign(align, ['full', 'wide']) ? width : '100%';
+		const heightAttr = !hasAlign(align, ['full', 'wide']) ? height : undefined;
+
+		if (props.attributes.storage === 'media' && props.attributes.mediaUrl) {
+			return (
+				<img
+					src={props.attributes.mediaUrl}
+					alt=""
+					style={style}
+					width={widthAttr}
+					height={heightAttr}
+				/>
+			);
+		}
+
+		return (
+			<svg
+				dangerouslySetInnerHTML={getSVG(props.attributes)}
+				width={widthAttr}
+				height={heightAttr}
+				style={style}
+			/>
+		);
+	};
 
 	return (
-		<div { ...blockProps } style={ wrapperProps }>
-			<OHMYSVG
-				style={ {
-					transform: getRotationCss( attributes.rotation ),
-				} }
-				attributes={ props.attributes }
-				svgData={ svgMarkup }
-			/>
+		<div
+			{...blockProps}
+			dangerouslySetInnerHTML={
+				href ? undefined : (props.attributes.storage === 'media' ? undefined : getSVG(props.attributes))
+			}
+		>
+			{href ? (
+				<a
+					href={href}
+					target={linkTarget}
+					rel={linkTarget ? NEW_TAB_REL : undefined}
+					title={title ?? undefined}
+					onClick={onclick}
+				>
+					<SvgTag />
+				</a>
+			) : (
+				props.attributes.storage === 'media' ? <SvgTag /> : null
+			)}
 		</div>
 	);
 };
